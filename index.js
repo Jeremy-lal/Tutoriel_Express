aeb1f6a68c6bd5ad39890b753d9d7ec38fbbff70
Jérémy LALAIT <jlalait@docone.fr>
2020-04-28 10:15:32 +0200

    feat: add put

--- index.js
+++ index.js
@@ -75,6 +75,31 @@ app.post('/movies', (req, res) => {
   });
 });
 
+app.put('/films', (req, res) => {
+  const bodyData = req.body;
+
+  connection.query('UPDATE movies SET ? WHERE id = ?', [bodydata, bodydata.id], err => {
+    if (err) {
+      res.status(500).send("Erreur de mise à jour d'un film");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+app.put('/films/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const bodyData = req.body;
+
+  connection.query('UPDATE movies SET ? WHERE id = ?', [bodydata, id], err => {
+    if (err) {
+      res.status(500).send("Erreur de mise à jour d'un film");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
 app.listen(port, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
