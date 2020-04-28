4790266f8cb8d6f295171495ec5ab15df9d1b3f2
Jérémy LALAIT <jlalait@docone.fr>
2020-04-28 09:57:24 +0200

    feat: add post

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const express = require('express');
+const connection = require('./mysql')
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3002;
 
-const connection = require('./mysql')
-const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true }));
+
+
+app.get('/films', (req, res) => {
+  connection.query('SELECT * from movies;', (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération des films');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/films/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  connection.query('SELECT * from movies WHERE id = ?', [id], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération du film');
+    } else if (results.length === 0) {
+      return res.status(404).send('Employee not found');
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+app.get('/films', (req, res) => {
+  let request = 'SELECT * FROM movies';
+  const querysValues = [];
+  if (req.query.category) {
+    request += ' WHERE category = ?';
+    querysValues.push(req.query.category);
+  }
+
+  connection.query(sql, querysValues, (err, results) => {
+    if (err) {
+      res.status(500).send(`An error occurred: ${err.message}`);
+    } else {
+      res.json(results);
+    }
+  });
+});
 
 app.post('/movies', (req, res) => {
   const bodyData = req.body;
@@ -17,7 +57,7 @@ app.post('/movies', (req, res) => {
       console.log(err);
       res.status(500).send("Erreur de sauvegarde de film");
     } else {
-      res.sendStatus(200);
+      res.sendStatus(201);
     }
   });
 });
