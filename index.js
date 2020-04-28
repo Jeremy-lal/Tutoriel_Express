494a212b467e1f6bc74041a9de15d78f635b6928
Jérémy LALAIT <jlalait@docone.fr>
2020-04-28 09:52:50 +0200

    feat: post

--- index.js
+++ index.js
@@ -3,48 +3,37 @@ const app = express();
 const port = 3002;
 
 const connection = require('./mysql')
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/films', (req, res) => {
-  connection.query('SELECT * from movies;', (err, results) => {
+app.post('/movies', (req, res) => {
+  const bodyData = req.body;
+
+  connection.query('INSERT INTO employee SET ?', bodyData, (err, results) => {
     if (err) {
-      res.status(500).send('Erreur lors de la récupération des films');
+      console.log(err);
+      res.status(500).send("Erreur de sauvegarde de film");
     } else {
-      res.json(results);
+      res.sendStatus(200);
     }
   });
 });
 
-app.get('/films/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
+app.post('/movies', (req, res) => {
+  const {category, limit} = req.body;
 
-  connection.query('SELECT * from movies WHERE id = ?', [id], (err, results) => {
+  connection.query('SELECT * FROM movies WHERE category = ? LIMIT ?', [category, limit], (err, results) => {
     if (err) {
-      res.status(500).send('Erreur lors de la récupération du film');
-    } else if (results.length === 0) {
-      return res.status(404).send('Employee not found');
+      console.log(err);
+      res.status(500).send("Erreur lors de la récupération de film");
     } else {
-      res.json(results[0]);
+      res.send(results);
     }
   });
 });
-
-app.get('/films', (req, res) => {
-  let request = 'SELECT * FROM movies';
-  const querysValues = [];
-  if (req.query.category) {
-    request += ' WHERE category = ?';
-    querysValues.push(req.query.category);
-  }
-
-  connection.query(sql, querysValues, (err, results) => {
-    if (err) {
-      res.status(500).send(`An error occurred: ${err.message}`);
-    } else {
-      res.json(results);
-    }
-  });
-});
-
 
 app.listen(port, (err) => {
   if (err) {
