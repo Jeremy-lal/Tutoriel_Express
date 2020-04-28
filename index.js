6ff6ed4c711e331bef1dcdd58399cbaa6769fa54
Jérémy LALAIT <jlalait@docone.fr>
2020-04-28 10:14:07 +0200

    feat: put

--- index.js
+++ index.js
@@ -8,72 +8,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
 
-app.get('/films', (req, res) => {
-  connection.query('SELECT * from movies;', (err, results) => {
-    if (err) {
-      res.status(500).send('Erreur lors de la récupération des films');
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-app.get('/films/:id', (req, res) => {
-  const id = parseInt(req.params.id, 10);
-
-  connection.query('SELECT * from movies WHERE id = ?', [id], (err, results) => {
-    if (err) {
-      res.status(500).send('Erreur lors de la récupération du film');
-    } else if (results.length === 0) {
-      return res.status(404).send('Employee not found');
-    } else {
-      res.json(results[0]);
-    }
-  });
-});
-
-app.get('/films', (req, res) => {
-  let request = 'SELECT * FROM movies';
-  const querysValues = [];
-  if (req.query.category) {
-    request += ' WHERE category = ?';
-    querysValues.push(req.query.category);
-  }
-
-  connection.query(sql, querysValues, (err, results) => {
-    if (err) {
-      res.status(500).send(`An error occurred: ${err.message}`);
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-app.post('/movies', (req, res) => {
+app.put('/films', (req, res) => {
   const bodyData = req.body;
 
-  connection.query('INSERT INTO employee SET ?', bodyData, (err, results) => {
+  connection.query('UPDATE movies SET ? WHERE id = ?', [bodydata, bodydata.id], err => {
     if (err) {
-      console.log(err);
-      res.status(500).send("Erreur de sauvegarde de film");
+      res.status(500).send("Erreur de mise à jour d'un film");
     } else {
-      res.sendStatus(201);
+      res.sendStatus(200);
     }
   });
 });
 
-app.post('/movies', (req, res) => {
-  const {category, limit} = req.body;
+app.put('/films/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const bodyData = req.body;
 
-  connection.query('SELECT * FROM movies WHERE category = ? LIMIT ?', [category, limit], (err, results) => {
+  connection.query('UPDATE movies SET ? WHERE id = ?', [bodydata, id], err => {
     if (err) {
-      console.log(err);
-      res.status(500).send("Erreur lors de la récupération de film");
+      res.status(500).send("Erreur de mise à jour d'un film");
     } else {
-      res.send(results);
+      res.sendStatus(200);
     }
   });
 });
+
+
 
 app.listen(port, (err) => {
   if (err) {
