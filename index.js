7fd7c853fb6e1caf8c173114e5a037feed606653
Jérémy LALAIT <jlalait@docone.fr>
2020-04-28 08:51:05 +0200

    feat: params and querys express

--- index.js
+++ index.js
@@ -14,6 +14,37 @@ app.get('/films', (req, res) => {
   });
 });
 
+app.get('/films/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  connection.query('SELECT * from movies WHERE id = ?', [id], (err, results) => {
+    if (err) {
+      res.status(500).send('Erreur lors de la récupération du film');
+    } else if (results.length === 0) {
+      return res.status(404).send('Employee not found');
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+app.get('/films', (req, res) => {
+  let request = 'SELECT * FROM movies';
+  const querysValues = [];
+  if (req.query.category) {
+    request += ' WHERE category = ?';
+    querysValues.push(req.query.category);
+  }
+
+  connection.query(sql, querysValues, (err, results) => {
+    if (err) {
+      res.status(500).send(`An error occurred: ${err.message}`);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 
 app.listen(port, (err) => {
   if (err) {
