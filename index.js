094ae4ef02613cc8d9c2c0b11e28c1c387a48540
Jérémy LALAIT <jlalait@docone.fr>
2020-04-28 10:31:56 +0200

    feat: add delete

--- index.js
+++ index.js
@@ -100,6 +100,18 @@ app.put('/films/:id', (req, res) => {
   });
 });
 
+app.delete('/films/:id', (req, res) => {
+  const id = req.params.id;
+
+  connection.query('DELETE FROM movies WHERE id = ?', [id], err => {
+    if (err) {
+      res.status(500).send("Erreur lors de la suppression d'un employé");
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
 app.listen(port, (err) => {
   if (err) {
     throw new Error('Something bad happened...');
