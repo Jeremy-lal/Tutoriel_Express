c4806bf92f36930cc49b894d9ce256b3016d6dd9
Jeremy-lal <jlalait33700@gmail.com>
2020-05-01 09:30:30 +0200

    feat: update

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const port = 3002;
 
 const connection = require('./mysql')
 
-app.get('/films', (req, res) => {
-  connection.query('SELECT * from movies;', (err, results) => {
-    if (err) {
-      res.status(500).send('Erreur lors de la récupération des films');
-    } else {
-      res.json(results);
-    }
-  });
-});
+// app.get('/movies', (req, res) => {
+//   connection.query('SELECT * from movies;', (err, results) => {
+//     if (err) {
+//       res.status(500).send('Erreur lors de la récupération des films');
+//     } else {
+//       res.json(results);
+//     }
+//   });
+// });
 
-app.get('/films/:id', (req, res) => {
+app.get('/movies/:id', (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   connection.query('SELECT * from movies WHERE id = ?', [id], (err, results) => {
@@ -28,7 +28,7 @@ app.get('/films/:id', (req, res) => {
   });
 });
 
-app.get('/films', (req, res) => {
+app.get('/movies', (req, res) => {
   let request = 'SELECT * FROM movies';
   const querysValues = [];
   if (req.query.category) {
@@ -36,7 +36,7 @@ app.get('/films', (req, res) => {
     querysValues.push(req.query.category);
   }
 
-  connection.query(sql, querysValues, (err, results) => {
+  connection.query(request, querysValues, (err, results) => {
     if (err) {
       res.status(500).send(`An error occurred: ${err.message}`);
     } else {
