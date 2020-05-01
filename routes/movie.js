ec0e443443df298a21e4622ddf0d1314d3122185
Jeremy-lal <jlalait33700@gmail.com>
2020-05-01 13:29:52 +0200

    feat: update

--- routes/movie.js
+++ routes/movie.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connection = require('../mysql')
 
 router.get('/', (req, res) => {
-    connection.query('SELECT * from movies;', (err, results) => {
+    connection.query('SELECT * from movie;', (err, results) => {
         if (err) {
             res.status(500).send('Erreur lors de la récupération des films');
         } else {
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const bodyData = req.body;
   
-    connection.query('INSERT INTO movies SET ?', bodyData, (err, results) => {
+    connection.query('INSERT INTO movie SET ?', bodyData, (err, results) => {
       if (err) {
         res.status(500).send("Erreur de sauvegarde de film");
       } else {
@@ -27,7 +27,7 @@ router.post('/', (req, res) => {
 router.put('/', (req, res) => {
     const bodyData = req.body;
 
-    connection.query('UPDATE movies SET ? WHERE id = ?', [bodyData, bodyData.id], err => {
+    connection.query('UPDATE movie SET ? WHERE id = ?', [bodyData, bodyData.id], err => {
         if (err) {
             res.status(500).send("Erreur de mise à jour d'un film");
         } else {
@@ -40,7 +40,7 @@ router.put('/', (req, res) => {
 router.delete('/:id', (req, res) => {
     const id = req.params.id;
 
-    connection.query('DELETE FROM movies WHERE id = ?', [id], err => {
+    connection.query('DELETE FROM movie WHERE id = ?', [id], err => {
         if (err) {
             res.status(500).send("Erreur lors de la suppression d'un employé");
         } else {
